f3b74e766bcc6f5630147135a380d0d2146e8a37
Seth Allen <seth.a@me.com>
2015-09-06 21:39:56 +0000

    redirect code didn't work as planned, reverting back to root_path redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
 	def create
 		current_user.posts.create(post_params)
-		redirect_to Post.find(params[:id])
+		redirect_to root_path
 	end
 
 	def show
